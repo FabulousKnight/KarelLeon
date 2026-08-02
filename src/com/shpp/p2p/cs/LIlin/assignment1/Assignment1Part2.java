9f254fb273fa4004e5cbe2ff549adcee038aea2a
FabulousKnight <tusk215@gmail.com>
2026-08-03 01:06:16 +0300

    InitialCommit

--- src/com/shpp/p2p/cs/LIlin/assignment1/Assignment1Part2.java
+++ src/com/shpp/p2p/cs/LIlin/assignment1/Assignment1Part2.java
@@ -5,23 +5,44 @@ import com.shpp.karel.KarelTheRobot;
 public class Assignment1Part2 extends KarelTheRobot {
 
     public void run() throws Exception {
-        puttingBeepersDiagonally();
+        fillingLineWithBeepers();
+        puttingBeepersInTheLastCell();
+        turnAround();
     }
 
-    private void puttingBeepersDiagonally() throws Exception {
-        putBeeper();
-        while(frontIsClear()){
-            turnLeft();
-            move();
-            turnRight();
-            move();
+    private void puttingBeepersInTheLastCell() throws Exception {
+        turnAround();
+        while (beepersPresent() && frontIsClear()) {
+            pickBeeper();
+            while (frontIsClear()) {
+                move();
+            }
             putBeeper();
+            comebackToTheSellWithBeepers();
         }
     }
 
-    private void turnRight() throws Exception {
-        turnLeft();
+    private void comebackToTheSellWithBeepers() throws Exception {
+        turnAround();
+        while (beepersPresent()) {
+            move();
+        }
+        turnAround();
+        move();
+    }
+
+    private void turnAround() throws Exception {
         turnLeft();
         turnLeft();
     }
+
+    private void fillingLineWithBeepers() throws Exception {
+        while (frontIsClear()) {
+            putBeeper();
+            move();
+        }
+        putBeeper();
+    }
+
+
 }
